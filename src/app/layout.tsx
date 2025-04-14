35a07c947b6cbe1610a3134576d48c82d66a1f5b
Sithira Senanayake <sithirasenanayake@gmail.com>
2025-04-14 10:06:01 +0530

    Added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProviders } from './theme-providers'
 import Footer from '@/components/Footer'
 import NavBar from '@/components/NavBar'
 import { Toaster } from 'sonner'
+import Analytics from '@/components/Analytics'
 
 const space_grotesk = Space_Grotesk({
   subsets: ['latin'],
@@ -70,7 +71,8 @@ export default function RootLayout({
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href={`${basePath}/feed.xml`} />
-      <body className="flex min-h-screen flex-col bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-gray-950 dark:text-white mr-2">
+      <body className="mr-2 flex min-h-screen flex-col bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-gray-950 dark:text-white">
+        <Analytics />
         <ThemeProviders>
           <section className="mx-auto flex w-full max-w-3xl flex-1 flex-col px-4 sm:px-6 xl:max-w-5xl xl:px-0">
             <NavBar />

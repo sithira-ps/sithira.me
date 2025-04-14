3144373ffba11a6071e2259e763ffb4d88947629
Sithira Senanayake <sithirasenanayake@gmail.com>
2025-04-14 21:52:41 +0530

    added light and dark mode button

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,41 @@
 // import Link from "./Link";
+'use client'
+
+import siteMetadata from "@/data/siteMetadata";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export default function Footer() {
+
+    const { theme, setTheme } = useTheme()
+    const [mounted, setMounted] = useState(false)
+
+    useEffect(() => {
+      setMounted(true)
+    }, [])
+
+    if (!mounted) return null 
+    
   return (
     <footer className="border-t-1 border-t-gray-300 dark:border-t-gray-800">
       <div className="mt-8 flex flex-col items-center">
-        <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
+        <div className="mb-2 flex items-center space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>Sithira Senanayake</div>
           <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
-          <div>{` • `}</div>
-          {/* <Link href="/">{siteMetadata.title}</Link> */}
-        </div>
-        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          {/* <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Tailwind Nextjs Theme
-          </Link> */}
+          <p className="mx-2">|</p>
+          {theme === 'dark' ? (
+            <Button variant="ghost" onClick={() => setTheme('light')}>
+              <Sun className="h-5 w-5" />
+            </Button>
+          ) : (
+            <Button variant="ghost" onClick={() => setTheme('dark')}>
+              <Moon className="h-5 w-5" />
+            </Button>
+          )}
         </div>
       </div>
     </footer>
